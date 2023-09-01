f6407681f4de809ccaa687c0dc4c2ab09fecc00b
lucumt <lucumt@gmail.com>
2023-09-01 17:05:52 +0800

    完善日志

--- views.py
+++ views.py
@@ -1,8 +1,12 @@
 import datetime
+
+import log
 import nacos_service as ns
 import yaml
 
 from flask import request
+
+logger = log.get_logger(__name__)
 
 
 def hello():
@@ -15,12 +19,14 @@ def get_config():
     result = dict(time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     config = ns.get_config("training_datasource_config.yml", "orienlink")
     result['config'] = yaml.safe_load(config)
+    logger.info("============config result " + str(result))
     return result
 
 
 def get_instance():
     service_name = request.args.get("service_name")
     result = ns.get_instance(service_name)
+    logger.info("============instance result " + str(result))
     return result
 
 
@@ -30,4 +36,5 @@ def invoke_instance():
     response = ns.invoke_instance(service, method)
     result = dict(time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     result['data'] = response
+    logger.info("============invoke result " + str(result))
     return result
